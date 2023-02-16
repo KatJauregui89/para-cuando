b779b5a79f7ddba060878c01db8629506ac60e90
RicardoSalazarR <axel.111yo@gmail.com>
2023-02-15 23:09:17 -0600

    build: layout created

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,19 @@
 import { Inter } from '@next/font/google';
+import type { ReactElement } from 'react'
+import Layout from '../components/layout'
+import type { NextPageWithLayout } from './_app'
 
+const Page: NextPageWithLayout = () => {
+  return <p>hello world</p>
+}
 const inter = Inter({ subsets: ['latin'] });
 
-export default function Home() {
+Page.getLayout = function getLayout(page: ReactElement) {
   return (
-    <>
+    <Layout>
       <div className="bg-red-200">HOME</div>
-    </>
-  );
+    </Layout>
+  )
 }
+
+export default Page
